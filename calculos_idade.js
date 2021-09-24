0b38a9283d4e2b2f356761a9f7657f64deb06b42
fabiana-chacon <71912919+fabiana-chacon@users.noreply.github.com>
2021-09-24 11:10:22 -0300

    Update calculos_idade.js

--- calculos_idade.js
+++ calculos_idade.js
@@ -2,13 +2,6 @@
 var titulo = document.querySelector(".titulo");
 
 
-//var data = document.getElementById('datadenasc').value
-//var ano_atual = new Date().getFullYear();
-//var data_Atual = ano_atual - parseInt(data);
-
-var data =document.getElementById('datadenasc').value
- var ver_data = data.substring(8,10);
-
 function calcIdade() {
   var data = document.getElementById('datadenasc').value
   var ano_atual = new Date().getFullYear();
@@ -47,16 +40,5 @@ function maior_Idade(){
 
 
 
-
-     
-      //var mes = data.substring(5,7);
-      //var ano = data.substring(0,4);
-
-      
-//var data_de_nascimento = ( dia+"/"+mes+"/"+ano) ;
-
-
-
-//return data_de_nascimento
 
 
